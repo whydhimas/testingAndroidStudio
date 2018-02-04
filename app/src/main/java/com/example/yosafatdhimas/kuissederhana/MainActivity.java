5fd9d8a078192b091105bf86a68cb15b8d1ed363
yosdhimas <yosdata@yahoo.com>
2018-02-04 13:55:23 +0700

    first push

---
+++ app/src/main/java/com/example/yosafatdhimas/kuissederhana/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.yosafatdhimas.kuissederhana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void yes_answer(View view) {
+        TextView tv = (TextView)findViewById(R.id.show_text);
+        tv.setText("YAK 100 JUTA RUPIAH");
+    }
+
+    public void no_answer(View view) {
+        TextView tv = (TextView)findViewById(R.id.show_text);
+        tv.setText(":(((");
+    }
+}
